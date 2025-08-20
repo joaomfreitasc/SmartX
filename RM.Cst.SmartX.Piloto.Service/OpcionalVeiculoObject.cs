b55ae121feeb4b3efe03021d96cde5362e849412
Joao Marcos Nascimento de Freitas <joao.mfreitas@totvs.com.br>
2025-08-20 16:27:08 -0300

    projeto inicial

---
+++ RM.Cst.SmartX.Piloto.Service/OpcionalVeiculoObject.cs
@@ -0,0 +1,48 @@
+﻿using RM.Lib.SmartX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RM.Cst.SmartX.Piloto.Service
+{
+    [SXObjectAttr("OpcionalVeiculoObject", "ZOPCIONALVEICULO", typeof(Properties.Resources), nameof(Properties.Resources.OpcionalVeiculoObject))]
+    [SXFinderAttr(new[] { nameof(CodColigada), nameof(Codigo) })]
+    internal class OpcionalVeiculoObject : SXObject
+    {
+        [SXPropertyAttr("CODCOLIGADA",
+            typeof(Properties.Resources),
+            nameof(Properties.Resources.CodigoDaColigada),
+            nameof(Properties.Resources.CodigoDaColigada),
+            Required = true,
+            //Hidden = true,
+            IsPrimaryKey = true)]
+        public int CodColigada { get; set; }
+
+        [SXPropertyAttr("CODIGO",
+            typeof(Properties.Resources),
+            nameof(Properties.Resources.Codigo),
+            nameof(Properties.Resources.Codigo),
+            Required = true,
+            //Hidden = true,
+            IsPrimaryKey = true)]
+        public int Codigo { get; set; }
+
+        [SXPropertyAttr("COD_OPCIONAL",
+            typeof(Properties.Resources),
+            nameof(Properties.Resources.CodigoOpcionalVeiculo),
+            nameof(Properties.Resources.CodigoOpcionalVeiculo),
+            Required = true,
+            IsPrimaryKey = true)]
+        public int Cod_OpcionalVeiculo { get; set; }
+
+        [SXPropertyAttr("COD_VEICULO",
+            typeof(Properties.Resources),
+            nameof(Properties.Resources.CodigoVeiculo),
+            nameof(Properties.Resources.CodigoVeiculo),
+            Required = true,
+            IsPrimaryKey = false)]
+        public int CodVeiculo { get; set; }
+    }
+}
---
+++ RM.Cst.SmartX.Piloto.Service/VeiculoObject.cs
@@ -0,0 +1,40 @@
+﻿/*using RM.Lib.SmartX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RM.Cst.SmartX.Piloto.Service
+{
+    [SXObjectAttr("MarcaObject", "ZMARCA", typeof(Properties.Resources), nameof(Properties.Resources.MarcaObject))]
+    //[SXFinderAttr(new[] { nameof(CodEmpresa), nameof(IdNota) })]
+    public class VeiculoObject : SXObject
+    {
+        [SXPropertyAttr("CODCOLIGADA",
+            typeof(Properties.Resources),
+            nameof(Properties.Resources.CodigoDaColigada),
+            nameof(Properties.Resources.CodigoDaColigada),
+            Required = true,
+            //Hidden = true,
+            IsPrimaryKey = true)]
+        public int CodColigada { get; set; }
+
+        [SXPropertyAttr("COD_MARCA",
+            typeof(Properties.Resources),
+            nameof(Properties.Resources.CodigoDaMarca),
+            nameof(Properties.Resources.CodigoDaMarca),
+            Required = true,
+            IsPrimaryKey = true)]
+        public int CodMarca { get; set; }
+
+        [SXPropertyAttr("DESCRICAO",
+            typeof(Properties.Resources),
+            nameof(Properties.Resources.Descricao),
+            nameof(Properties.Resources.Descricao),
+            Required = true,
+            IsPrimaryKey = false)]
+        public int Descricao { get; set; }
+    }
+}
+*/
